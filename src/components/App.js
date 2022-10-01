0eec828622a95d1df07b51094c43e4332e73ccb1
AleksandrukZakhar <zakharaleksandrukdev@gmail.com>
2022-10-01 11:38:56 +0300

    Add Waldo background and found feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,30 @@
-const App = () => null;
+import { useState, useEffect } from "react";
+import Background from "../assets/background.png";
+import Waldo from "../assets/waldo.png";
+
+const App = () => {
+    const [found, setFound] = useState(false);
+    const [time, setTime] = useState(0);
+
+    useEffect(() => {
+        if (!found) {
+            setTimeout(() => {
+                setTime((prev) => prev + 1);
+            }, [1000]);
+        }
+    });
+
+    return (
+        <>
+            <img src={Background} id="background" alt="background" />{" "}
+            <img
+                src={Waldo}
+                id="waldo"
+                alt="waldo"
+                onClick={() => setFound(true)}
+            />
+        </>
+    );
+};
 
 export default App;
