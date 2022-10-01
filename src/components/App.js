5100ba37bad0ffa7e698777e8382010b29192b5e
AleksandrukZakhar <zakharaleksandrukdev@gmail.com>
2022-10-01 17:04:30 +0300

    Sort leaders list

--- src/components/App.js
+++ src/components/App.js
@@ -77,7 +77,9 @@ const App = () => {
                     setShowLeaders={setShowLeaders}
                 />
             ) : null}
-            {showLeaders ? <Leaders collectionRef={collectionRef} /> : null}
+            {showLeaders ? (
+                <Leaders collectionRef={collectionRef} toHHMMSS={toHHMMSS} />
+            ) : null}
         </>
     );
 };
--- src/components/Leaders.js
+++ src/components/Leaders.js
@@ -1,6 +1,6 @@
 import { useCollection } from "react-firebase-hooks/firestore";
 
-const Leaders = ({ collectionRef }) => {
+const Leaders = ({ collectionRef, toHHMMSS }) => {
     const [snapshot, loading, error] = useCollection(collectionRef);
 
     return (
@@ -8,13 +8,27 @@ const Leaders = ({ collectionRef }) => {
             <div className="leaders">
                 <h1>Leaders</h1>
                 {error ? <p>Something went wrong</p> : null}
-                {snapshot?.docs?.map((doc, index) => {
-                    return (
-                        <p key={Math.floor(Math.random() * 10000000)}>
-                            {index}. {doc.data().nick} {doc.data().time}
-                        </p>
-                    );
-                })}
+                {snapshot?.docs
+                    ?.map((doc) => {
+                        const { nick, time } = doc.data();
+
+                        return { nick, time };
+                    })
+                    .sort((a, b) => {
+                        return a.time - b.time;
+                    })
+                    .filter((value, index) => {
+                        return index < 10;
+                    })
+                    .map((user, index) => {
+                        const { nick, time } = user;
+
+                        return (
+                            <p key={Math.floor(Math.random() * 100000)}>
+                                {index + 1}. {nick} {toHHMMSS(time)}{" "}
+                            </p>
+                        );
+                    })}
             </div>
         </div>
     );
